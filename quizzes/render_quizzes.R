c2f66f6d37ed3083ddca3efa3a44f1e2b8cd5aa5
Kene David Nwosu <kendavidn@gmail.com>
2025-03-14 15:51:01 +0000

    python quizzes done!

--- quizzes/render_quizzes.R
+++ quizzes/render_quizzes.R
@@ -1,8 +1,8 @@
-# FOR THEORY QUIZZES: Use the code below. Function is stored on a github gist. 
+# FOR THEORY QUIZZES: Use the code below. Function is stored on a github gist.
 
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## Connect to GDrive ----
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 pacman::p_load(googledrive)
 # Setup for uploading images and datasets
@@ -13,13 +13,32 @@ options(gargle_oauth_email = "trainingteam@thegraphnetwork.org")
 pacman::p_load(devtools)
 devtools::source_gist("https://gist.github.com/kendavidn/05c7055e487ef22e5a336a4cb489a937")
 
+# Pandoc
+# Set the PATH environment variable for VSCode
+Sys.setenv(
+  PATH = paste(
+    "/Users/kendavid/opt/anaconda3/bin",
+    "/opt/homebrew/bin",
+    Sys.getenv("PATH"),
+    sep = ":"
+  )
+)
 
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## Enter quiz information ----
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-quiz_path <- here::here("quizzes/module_c2_mcq.Rmd")
+# Get all quiz files starting with p_mcq in the quizzes folder
+quiz_files <- list.files(
+  path = here::here("quizzes"),
+  pattern = "^p_mcq.*\\.Rmd$",
+  full.names = TRUE
+)
 
-process_theory_quiz(quiz_path)
-
-
+# Process each quiz file
+for (i in 1:length(quiz_files)) {
+  i = 15
+  print(paste("Processing quiz", i, "of", length(quiz_files), "which is", quiz_files[i]))
+  process_theory_quiz(quiz_files[i])
+}
